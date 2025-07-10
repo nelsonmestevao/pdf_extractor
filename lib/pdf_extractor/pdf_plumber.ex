1001f7db80ed628539ce310e4481318ae70b773d
christopher.eyre <christopher.eyre@prima.it>
2025-07-10 14:39:14 +0100

    Add the ability to parse from a binary

--- lib/pdf_extractor/pdf_plumber.ex
+++ lib/pdf_extractor/pdf_plumber.ex
@@ -24,7 +24,7 @@ defmodule PdfExtractor.PdfPlumber do
     extract_text(file_path, List.wrap(page_number), areas)
   end
 
-  def extract_text(file_path, page_numbers, areas) when is_list(page_numbers) and is_map(areas) do
+  def extract_text(body, page_numbers, areas) when is_list(page_numbers) and is_map(areas) do
     """
     import pdfplumber
     import logging
@@ -51,7 +51,51 @@ defmodule PdfExtractor.PdfPlumber do
     main(file_path.decode('utf-8'), page_numbers, areas)
     """
     |> Pythonx.eval(%{
-      "file_path" => file_path,
+      "body" => body,
+      "page_numbers" => page_numbers,
+      "areas" => areas
+    })
+    |> elem(0)
+    |> Pythonx.decode()
+    |> to_map(page_numbers)
+  end
+
+  @doc """
+    This version avoids the need to put the pdf on a filesystem.
+    This allows this to work
+    url |> :httpc.request() |> elem(1) |> elem(2) |> :binary.list_to_bin() |> PdfExtractor.extract_text_from_url()
+  """
+  def extract_text_from_binary(binary, page_numbers, areas)
+      when is_list(page_numbers) and is_map(areas) do
+    """
+    import pdfplumber
+    import logging
+    from io import BytesIO
+
+    logging.getLogger("pdfminer").setLevel(logging.ERROR)
+
+    def extract_from_page(page, area=None):
+        if area:
+            return page.within_bbox(area).extract_text()
+        else:
+            return page.extract_text()
+
+    def main(binary, page_numbers, areas):
+        results = []
+
+        with pdfplumber.open(BytesIO(binary)) as pdf:
+            total_pages = len(pdf.pages)
+            if page_numbers == []:
+              page_numbers = list(range(total_pages))
+            for page_number in page_numbers:
+              if page_number >= 0 and page_number < total_pages:
+                results.append(extract_from_page(pdf.pages[page_number], areas.get(page_number)))
+            return results
+
+    main(binary, page_numbers, areas)
+    """
+    |> Pythonx.eval(%{
+      "url_body" => url_body,
       "page_numbers" => page_numbers,
       "areas" => areas
     })
