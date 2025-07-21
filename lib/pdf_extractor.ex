63a5ecca4ab00c4ea145907bc037668cbfb7db15
Nelson Estevão <nelsonmestevao@proton.me>
2025-07-21 01:10:48 +0100

    doc: improve examples formatting

--- lib/pdf_extractor.ex
+++ lib/pdf_extractor.ex
@@ -28,19 +28,29 @@ defmodule PdfExtractor do
 
   ## Examples
 
-      # Extract text from all pages
-      iex> PdfExtractor.extract_text("priv/fixtures/fatura.pdf")
-      %{0 => "Text Example Bill FATURA\n# 2025010002\nData: Jun 21, 2025\nProjeto de lei para:\nSaldo devedor: 1 525,59 €\nElixir Company\nItem Quantidade Avaliar Quantia\nTrabalho 1 1 500,00 € 1 500,00 €\nMais trabalho 1 25,59 € 25,59 €\nSubtotal: 1 525,59 €\nImposto (0%): 0,00 €\nTotal: 1 525,59 €", 1 => "✂\nReceipt Payment part Account / Payable to\nCH4431999123000889012\n✂\nMax Muster & Söhne\nAccount / Payable to\nCH4431999123000889012 Musterstrasse 123\nMax Muster & Söhne 8000 Seldwyla\nMusterstrasse 123\n8000 Seldwyla\nReference\n210000000003139471430009017\nReference\n210000000003139471430009017\nAdditional information\nBestellung vom 15.10.2020\nPayable by (name/address)\nSimon Muster\nPayable by (name/address)\nMusterstrasse 1\nCurrency Amount\nSimon Muster\n8000 Seldwyla\nCHF 1 949.75 Musterstrasse 1\n8000 Seldwyla\nCurrency Amount\nCHF 1 949.75\nAcceptance point" }
+    Extract text from all pages.
 
-      # Extract only the titles in the book chapters
+      iex> PdfExtractor.extract_text("priv/fixtures/fatura.pdf")
+      %{
+        0 => "Text Example Bill FATURA\n# 2025010002\nData: Jun 21, 2025\nProjeto de lei para:\nSaldo devedor: 1 525,59 €\nElixir Company\nItem Quantidade Avaliar Quantia\nTrabalho 1 1 500,00 € 1 500,00 €\nMais trabalho 1 25,59 € 25,59 €\nSubtotal: 1 525,59 €\nImposto (0%): 0,00 €\nTotal: 1 525,59 €", 
+        1 => "✂\nReceipt Payment part Account / Payable to\nCH4431999123000889012\n✂\nMax Muster & Söhne\nAccount / Payable to\nCH4431999123000889012 Musterstrasse 123\nMax Muster & Söhne 8000 Seldwyla\nMusterstrasse 123\n8000 Seldwyla\nReference\n210000000003139471430009017\nReference\n210000000003139471430009017\nAdditional information\nBestellung vom 15.10.2020\nPayable by (name/address)\nSimon Muster\nPayable by (name/address)\nMusterstrasse 1\nCurrency Amount\nSimon Muster\n8000 Seldwyla\nCHF 1 949.75 Musterstrasse 1\n8000 Seldwyla\nCurrency Amount\nCHF 1 949.75\nAcceptance point"
+      }
+
+    Extract only the titles in the book chapters.
+
       iex> PdfExtractor.extract_text("priv/fixtures/book.pdf", [2, 8, 10], %{
       ...>   2 => {0, 0, 612, 190},
       ...>   8 => {0, 0, 612, 190},
       ...>   10 => {0, 0, 612, 190}
       ...> })
-      %{2 => "Introdução – Nota do tradutor", 8 => "I. Sobre aproveitar o tempo", 10 => "II. Sobre a falta de foco na Leitura"}
+      %{
+        2 => "Introdução – Nota do tradutor",
+        8 => "I. Sobre aproveitar o tempo", 
+       10 => "II. Sobre a falta de foco na Leitura"
+      }
 
-      # Extract multiple areas from a single page
+    Extract multiple areas from a single page.
+
       iex> PdfExtractor.extract_text("priv/fixtures/book.pdf", 1, %{
       ...>   1 => [{0, 100, 612, 140}, {0, 400, 612, 440}]
       ...> })
@@ -58,26 +68,38 @@ defmodule PdfExtractor do
 
   ## Examples
 
-      # Extract text from all pages
+    Extract text from all pages.
+
       iex> content = File.read!("priv/fixtures/fatura.pdf")
       iex> PdfExtractor.extract_text_from_binary(content)
-      %{0 => "Text Example Bill FATURA\n# 2025010002\nData: Jun 21, 2025\nProjeto de lei para:\nSaldo devedor: 1 525,59 €\nElixir Company\nItem Quantidade Avaliar Quantia\nTrabalho 1 1 500,00 € 1 500,00 €\nMais trabalho 1 25,59 € 25,59 €\nSubtotal: 1 525,59 €\nImposto (0%): 0,00 €\nTotal: 1 525,59 €", 1 => "✂\nReceipt Payment part Account / Payable to\nCH4431999123000889012\n✂\nMax Muster & Söhne\nAccount / Payable to\nCH4431999123000889012 Musterstrasse 123\nMax Muster & Söhne 8000 Seldwyla\nMusterstrasse 123\n8000 Seldwyla\nReference\n210000000003139471430009017\nReference\n210000000003139471430009017\nAdditional information\nBestellung vom 15.10.2020\nPayable by (name/address)\nSimon Muster\nPayable by (name/address)\nMusterstrasse 1\nCurrency Amount\nSimon Muster\n8000 Seldwyla\nCHF 1 949.75 Musterstrasse 1\n8000 Seldwyla\nCurrency Amount\nCHF 1 949.75\nAcceptance point"}
+      %{
+        0 => "Text Example Bill FATURA\n# 2025010002\nData: Jun 21, 2025\nProjeto de lei para:\nSaldo devedor: 1 525,59 €\nElixir Company\nItem Quantidade Avaliar Quantia\nTrabalho 1 1 500,00 € 1 500,00 €\nMais trabalho 1 25,59 € 25,59 €\nSubtotal: 1 525,59 €\nImposto (0%): 0,00 €\nTotal: 1 525,59 €", 
+        1 => "✂\nReceipt Payment part Account / Payable to\nCH4431999123000889012\n✂\nMax Muster & Söhne\nAccount / Payable to\nCH4431999123000889012 Musterstrasse 123\nMax Muster & Söhne 8000 Seldwyla\nMusterstrasse 123\n8000 Seldwyla\nReference\n210000000003139471430009017\nReference\n210000000003139471430009017\nAdditional information\nBestellung vom 15.10.2020\nPayable by (name/address)\nSimon Muster\nPayable by (name/address)\nMusterstrasse 1\nCurrency Amount\nSimon Muster\n8000 Seldwyla\nCHF 1 949.75 Musterstrasse 1\n8000 Seldwyla\nCurrency Amount\nCHF 1 949.75\nAcceptance point"
+      }
 
-      # Extract only the titles in the book chapters
+    Extract only the titles in the book chapters.
+
       iex> content = File.read!("priv/fixtures/book.pdf")
       iex> PdfExtractor.extract_text_from_binary(content, [2, 8, 10], %{
       ...>   2 => {0, 0, 612, 190},
       ...>   8 => {0, 0, 612, 190},
       ...>   10 => {0, 0, 612, 190}
       ...> })
-      %{2 => "Introdução – Nota do tradutor", 8 => "I. Sobre aproveitar o tempo", 10 => "II. Sobre a falta de foco na Leitura"}
+      %{
+        2 => "Introdução – Nota do tradutor",
+        8 => "I. Sobre aproveitar o tempo", 
+       10 => "II. Sobre a falta de foco na Leitura"
+      }
 
-      # Extract multiple areas from a single page
+    Extract multiple areas from a single page.
+
       iex> content = File.read!("priv/fixtures/book.pdf")
       iex> PdfExtractor.extract_text_from_binary(content, 1, %{
       ...>   1 => [{0, 100, 612, 140}, {0, 400, 612, 440}]
       ...> })
-      %{1 => ["CARTAS DE UM ESTOICO, Volume I", "Montecristo Editora Ltda.\ne-mail: editora@montecristoeditora.com.br"]}
+      %{
+        1 => ["CARTAS DE UM ESTOICO, Volume I", "Montecristo Editora Ltda.\ne-mail: editora@montecristoeditora.com.br"]
+       }
 
   """
   defdelegate extract_text_from_binary(binary, page_numbers \\ [], areas \\ %{}),
