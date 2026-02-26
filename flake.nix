fb2d812dbec27a47de20086301a563145e55a53d
Nelson Estevão <nelsonmestevao@proton.me>
2026-02-26 21:14:24 +0000

    chore: upgrade dependencies
    Upgraded:
      credo 1.7.12 => 1.7.16
      dialyxir 1.4.6 => 1.4.7
      doctest_formatter 0.4.0 => 0.4.1
      erlex 0.2.7 => 0.2.8
      ex_doc 0.38.3 => 0.40.1 (minor)
      file_system 1.1.0 => 1.1.1
      makeup_erlang 1.0.2 => 1.0.3
      styler 1.7.0 => 1.11.0

--- flake.nix
+++ flake.nix
@@ -2,26 +2,40 @@
   description = "Development Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    beam-flakes = {
+      url = "github:elixir-tools/nix-beam-flakes";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = false;
-        };
-      in {
-        devShells.default = pkgs.mkShell {
-          name = "pdfextractor";
+  outputs = inputs @ {
+    beam-flakes,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [beam-flakes.flakeModule];
 
-          buildInputs = with pkgs; [
-            beam28Packages.elixir_1_19
-            beam28Packages.erlang
-          ];
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+
+        beamWorkspace = {
+          enable = true;
+          devShell = {
+            extraPackages = with pkgs; [
+            ];
+          };
+          versions.fromToolVersions = ./.tool-versions;
         };
-      }
-    );
+      };
+    };
 }
