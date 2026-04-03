0932c37836540fd165d976bfc0135eeeb9f639db
Nelson Estevão <nelsonmestevao@proton.me>
2026-04-03 14:50:42 +0100

    chore: release it to hex

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PdfExtractor.MixProject do
 
   @app :pdf_extractor
   @name "PdfExtractor"
-  @version "0.5.1"
+  @version "0.6.0"
   @source_url "https://github.com/nelsonmestevao/pdf_extractor"
 
   def project do
